1bd80668d04ba16af528346334a11d6813f0af55
karterisg <sdi2100258@di.uoa.gr>
2025-12-09 08:43:15 +0200

    first ml imports

---
+++ src/ml/salary_predictor.py
@@ -0,0 +1,31 @@
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+
+df = pd.read_csv("data/clean_data.csv")
+
+X = df [['age']]
+y = df['salary']                    
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+
+model = LinearRegression()
+
+model.fit(X_train, y_train)
+
+preds = model.predict(X_test)
+
+mse = mean_squared_error(y_test, preds)
+
+print("Predictions:", preds)
+print("Mean Squared Error:", mse)
+print("True values:", list(y_test))
+print("Coefficient (slope):", model.coef_) #  coefficients
+print("Intercept:", model.intercept_)  #  coefficients
+
